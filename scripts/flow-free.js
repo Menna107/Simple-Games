a0e78a116cd14c16ee50d14cbb11918ee218a332
Menna <menna0hekal2014@gmail.com>
2025-06-18 23:53:50 +0300

    new update

--- scripts/flow-free.js
+++ scripts/flow-free.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 const cellSize = 60;
-const rows = 8;
+const rows = 7;
 const cols = 5;
 
 let dragging = false;
